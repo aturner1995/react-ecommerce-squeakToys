9c5a63d700eab23ab71e543f894feed8991956ec
Aaron <aaturner1995@gmail.com>
2023-03-27 18:19:53 -0300

    added bootstrap cards for the featured products. Added second image that shows on the hover

---
+++ src/components/Card/ProductCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './ProductCard.css';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+
+const ProductCard = ({ item }) => {
+  return (
+    <Link to={`/product/${item.id}`}>
+
+      <Card style={{ width: '18rem' }}>
+        <div className="image">
+          <Card.Img variant="top" src={item.img} className='mainImage' />
+          <Card.Img variant="top" src={item.img2} className='secondImage' />
+        </div>
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          <div className="prices">
+            <Card.Text className='oldPrice'>
+              {item.oldPrice}
+            </Card.Text>
+            <Card.Text className='newPrice'>
+              {item.newPrice}
+            </Card.Text>
+          </div>
+        </Card.Body>
+      </Card>
+    </Link>
+  )
+}
+
+export default ProductCard
--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './FeaturedProducts.css';
+import ProductCard from '../Card/ProductCard';
 
-const FeaturedProducts = (type) => {
+const FeaturedProducts = ({type}) => {
     const data = [
         {
             id: 1,
-            img: '',
-            img2: '',
-            title: '',
+            img: 'https://www.petvalu.ca/ccstore/v1/images/?source=/file/v6753388337233359265/products/SCM09680CA.1.jpg&height=300&width=300',
+            img2: 'https://www.petvalu.ca/ccstore/v1/images/?source=/file/v4568606820089954571/products/SCM09680CA.2.jpg&height=475&width=475',
+            title: 'Big Fish Squeak Toy',
             isNew: true,
             oldPrice: 49.99,
             newPrice: 39.99
         },
         {
-            id: 1,
-            img: '',
-            img2: '',
-            title: '',
+            id: 2,
+            img: 'https://www.petvalu.ca/ccstore/v1/images/?source=/file/v4136036574776777032/products/SCM01348CA.1.jpg&height=475&width=475',
+            img2: 'https://www.petvalu.ca/ccstore/v1/images/?source=/file/v5064066487747555899/products/SCM01348CA.3.jpg&height=475&width=475',
+            title: 'Teddy Bear Dog Toy',
             isNew: true,
             oldPrice: 49.99,
             newPrice: 39.99
         },
         {
-            id: 1,
+            id: 3,
             img: '',
             img2: '',
-            title: '',
+            title: 'Big Chew Toy',
             isNew: true,
             oldPrice: 49.99,
             newPrice: 39.99
         },
         {
-            id: 1,
+            id: 4,
             img: '',
             img2: '',
             title: '',
@@ -42,10 +44,13 @@ const FeaturedProducts = (type) => {
   return (
     <div className='featuredProducts'>
         <div className="top">
-            <h1>Featured Products</h1>
+            <h1>{type}Featured Products</h1>
         </div>
         <div className="bottom">
-
+            {data.map(item => (
+                <ProductCard item={item} key={item.id}/>
+            )
+            )}            
         </div>
     </div>
   )

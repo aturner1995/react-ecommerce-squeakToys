c60889312c7dda2b133a5494f488c98fdd4102fc
Aaron <aaturner1995@gmail.com>
2023-04-05 20:39:54 -0300

    fixed heroku

--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 dotenv.config();
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2020-08-27' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const stripeRouter = express.Router();
 stripeRouter.use(cors());
 
